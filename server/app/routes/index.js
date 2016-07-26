95a644d5a23b2a74c7671458e992191b9284b888
rz-roy <rz.royzhang@gmail.com>
2016-07-26 16:09:59 -0400

    updated routes index

--- server/app/routes/index.js
+++ server/app/routes/index.js
@@ -3,6 +3,11 @@ var router = require('express').Router();
 module.exports = router;
 
 router.use('/members', require('./members'));
+router.use('/buildings', require('./buildings'));
+router.use('/carts', require('./carts'));
+router.use('/orders', require('./orders'));
+router.use('/reviews', require('./reviews'));
+router.use('/users', require('./users'));
 
 // Make sure this is after all of
 // the registered routes!
