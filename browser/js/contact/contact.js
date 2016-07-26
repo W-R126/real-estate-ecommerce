bb8d7fbfeaee3c36994dac17b90f35cf92ca0f9b
rz-roy <rz.royzhang@gmail.com>
2016-07-26 15:12:45 -0400

    working on contact page

---
+++ browser/js/contact/contact.js
@@ -0,0 +1,14 @@
+app.config(function ($stateProvider) {
+
+    $stateProvider.state('contact', {
+        url: '/contact',
+        templateUrl: 'js/contact/contact.html'
+    });
+
+});
+
+// app.controller('ContactController', function ($scope) {
+//   $scope.images = _.shuffle(FullstackPics);
+
+
+// });
