7475b2c79849900feefc0a22b9335b64588df083
rz-roy <rz.royzhang@gmail.com>
2016-07-27 18:40:31 -0400

    Stuck on stateParams

---
+++ browser/js/home/SearchFactory.js
@@ -0,0 +1,16 @@
+app.factory('SearchFactory', function($http){
+  var SearchFactoryObj = {};
+
+  SearchFactoryObj.getTypes = function(){
+    return $http.get('/api/buildings/types/')
+    .then(function(types){return types.data});
+  }
+
+  SearchFactoryObj.searchFields = function(obj){
+
+    return $http.get('/api/buildings/?'+"propertyType="+obj.propertyType)
+    .then(buildings=>buildings.data);
+  }
+
+  return SearchFactoryObj;
+})
--- browser/js/search-results/search-results.js
+++ browser/js/search-results/search-results.js
@@ -2,10 +2,16 @@ app.config(function($stateProvider){
   $stateProvider.state('search-results', {
     url: '/buildings',
     templateUrl: 'js/search-results/search-results.html',
-    controller: 'ResultsCtrl'
+    controller: 'ResultsCtrl',
+    resolve: {
+      theBuildings: function($stateParams){
+        console.log("****BUILDINGS***", $stateParams);
+        return $stateParams.theBuildings;
+      }
+    }
   })
 })
 
-app.controller('ResultsCtrl', function($scope){
-
+app.controller('ResultsCtrl', function($scope, theBuildings){
+  $scope.buildings = theBuildings;
 })
