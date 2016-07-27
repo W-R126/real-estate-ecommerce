21d1333da356e7be50755c19cb737ccd65296615
rz-roy <rz.royzhang@gmail.com>
2016-07-27 12:44:21 -0400

    added cart view

---
+++ browser/js/cart/cart.js
@@ -0,0 +1,16 @@
+app.config(function ($stateProvider) {
+
+    // Register our *about* state.
+    $stateProvider.state('cart', {
+        url: '/cart',
+        controller: 'CartController',
+        templateUrl: 'js/cart/cart.html'
+    });
+
+});
+
+app.controller('CartController', function ($scope) {
+
+
+
+});
