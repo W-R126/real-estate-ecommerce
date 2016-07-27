bc32dd017c3608d74538d33bfceb076fe18605fe
rz-roy <rz.royzhang@gmail.com>
2016-07-27 16:09:04 -0400

    Update signup.factory.js

--- browser/js/signup/signup.factory.js
+++ browser/js/signup/signup.factory.js
@@ -3,7 +3,6 @@ app.factory('UserFactory', function ($http) {
   var UserFactory = {};
 
   UserFactory.create = function (data) {
-    console.log('*************', data)
     return $http.post('/api/users/', data)
     .then(function (response) { return response.data; })
     .catch(function(error){console.error(error);
