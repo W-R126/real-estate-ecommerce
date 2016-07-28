faa0d42cbac2740f9dbd6db07a8acefd662404d9
rz-roy <rz.royzhang@gmail.com>
2016-07-28 17:02:58 -0400

    refactoring buildings, load issue

--- browser/js/building-detail/building-detail.js
+++ browser/js/building-detail/building-detail.js
@@ -23,58 +23,18 @@ app.config(function ($stateProvider) {
 // all buildings page
 app.config(function ($stateProvider) {
   $stateProvider.state('buildings', {
-    url: '/properties',
+    url: '/properties?type',
     templateUrl: 'js/building-detail/templates/buildings.html',
     controller: 'BuildingsCtrl',
     resolve:{
-      allBuildings: function (BuildingFactory){
-        return BuildingFactory.fetchAll();
+      allBuildings: function (BuildingFactory, $stateParams){
+        console.log("Type:", $stateParams.type);
+        return BuildingFactory.fetchAll({propertyType: $stateParams.type});
       }
     }
   })
 })
 
-// commercial buildings page
-app.config(function ($stateProvider) {
-  $stateProvider.state('buildingscommercial', {
-    url: '/properties/commercial',
-    templateUrl: 'js/building-detail/templates/buildings.html',
-    controller: 'BuildingsCommercialCtrl',
-    resolve:{
-      allCommercialBuildings: function (BuildingFactory){
-        return BuildingFactory.fetchAllCommercial();
-      }
-    }
-  })
-})
-
-// residential buildings page
-app.config(function ($stateProvider) {
-  $stateProvider.state('buildingsresidential', {
-    url: '/properties/residential',
-    templateUrl: 'js/building-detail/templates/buildings.html',
-    controller: 'BuildingsCommercialCtrl',
-    resolve:{
-      allCommercialBuildings: function (BuildingFactory){
-        return BuildingFactory.fetchAllResidential();
-      }
-    }
-  })
-})
-
-// mixed buildings page
-app.config(function ($stateProvider) {
-  $stateProvider.state('buildingsmixed', {
-    url: '/properties/mixed',
-    templateUrl: 'js/building-detail/templates/buildings.html',
-    controller: 'BuildingsMixedCtrl',
-    resolve:{
-      allMixedBuildings: function (BuildingFactory){
-        return BuildingFactory.fetchAllMixed();
-      }
-    }
-  })
-})
 
 //single building page
 app.config(function ($stateProvider) {
@@ -95,15 +55,4 @@ app.controller('BuildingsCtrl', function($scope, allBuildings){
   $scope.buildings = allBuildings;
 })
 
-app.controller('BuildingsCommercialCtrl', function($scope, allCommercialBuildings){
-  $scope.buildings = allCommercialBuildings;
-})
-
-app.controller('BuildingsResidentialCtrl', function($scope, allResidentialBuildings){
-  $scope.buildings = allResidentialBuildings;
-})
-
-app.controller('BuildingsMixedCtrl', function($scope, allMixedBuildings){
-  $scope.buildings = allMixedBuildings;
-})
 
--- server/app/routes/buildings.js
+++ server/app/routes/buildings.js
@@ -4,39 +4,6 @@ var router = new express.Router();
 var db = require('../../db');
 var Building = db.Building;
 
-router.get('/commercial', function(req, res, next) {
-  Building.findAll({
-    where: {
-      propertyType: 'Commercial'
-    }
-  })
-  .then(commercialBuildings => res.send(commercialBuildings))
-  .catch(err=>console.error);
-})
-
-router.get('/residential', function(req, res, next) {
-  Building.findAll({
-    where: {
-      propertyType: 'Residential'
-    }
-  })
-  .then(commercialBuildings => res.send(commercialBuildings))
-  .catch(err=>console.error);
-})
-
-router.get('/mixed', function(req, res, next) {
-  Building.findAll({
-    where: {
-      propertyType: 'Mixed'
-    }
-  })
-  .then(mixedBuildings => res.send(mixedBuildings))
-  .catch(err=>console.error);
-})
-
-router.get('/types', function(req, res, next){
-  res.send(Building.rawAttributes.propertyType.values);
-})
 
 router.get('/:id', function(req, res, next){
 
@@ -46,6 +13,7 @@ router.get('/:id', function(req, res, next){
 })
 
 router.get('/', function(req, res, next){
+  conole.log("******Query", req.query);
   Building.findAll({where:req.query})
   .then(buildings=>res.send(buildings))
   .catch(err=>console.error);
