0b6d2644adc72c47daed95f60a85bb954ff29845
rz-roy <rz.royzhang@gmail.com>
2016-07-31 18:38:36 -0400

    Add functioning test message with local file based validation

--- server/app/routes/orders.js
+++ server/app/routes/orders.js
@@ -2,19 +2,22 @@ var express = require('express');
 var router = new express.Router();
 var fs = require('fs');
 
-var secretMG= fs.readSync('../../../../secretMG.txt',   'utf8');
+var secretMG= JSON.parse(fs.readFileSync('/home/barry/secretMG.txt','utf8'));
 var message = {
-  from:
-  to: '',
-  text: "Your order from Betty's Building Bros has been received!",
-  html:'<b> Text Contents</b>'
+  from: 'sandbox@mailgun.org',
+  to: 'bjw2119@gmail.com',
+  text: "Your order from Betty's Building Bros has been received!"
 }
 
 var nodemailer = require('nodemailer');
 var transport = {
-  host:
+  service:'Mailgun',
+  auth: {
+    user: secretMG.user,
+    pass: secretMG.pass
+  }
 }
-var transporter = nodemailer.createTransport()
+var transporter = nodemailer.createTransport(transport);
 
 var db = require('../../db');
 var Order = db.Order;
@@ -50,10 +53,12 @@ router.get('/', function(req, res, next){
 });
 
 router.post('/', function(req, res, next){
+  console.log("Testing conf email...user: "+ secretMG.user+"pass "+secretMG.pass);
   transporter.sendMail(message, function(error, info){
     if (error) console.log("Confirmation Mail Error: ",error);
     else console.log('Sent: '+ info.response);
-  })
+  });
+  res.sendStatus(200)
 })
 
 module.exports = router;
