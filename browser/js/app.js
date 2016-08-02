c3df886d52b673d071555ab420827bfbac5b2b2a
rz-roy <rz.royzhang@gmail.com>
2016-08-02 11:11:15 -0400

    success on changeState will scroll to the top of the page

--- browser/js/app.js
+++ browser/js/app.js
@@ -20,6 +20,11 @@ app.run(function ($rootScope, AuthService, $state) {
         return state.data && state.data.authenticate;
     };
 
+$rootScope.$on("$stateChangeSuccess", function (event, currentRoute, previousRoute) {
+    window.scrollTo(0, 0);
+});
+
+
     // $stateChangeStart is an event fired
     // whenever the process of changing a state begins.
     $rootScope.$on('$stateChangeStart', function (event, toState, toParams) {
